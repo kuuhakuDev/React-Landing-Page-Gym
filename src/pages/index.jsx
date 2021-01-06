c4e198f968bc41c48f6d04b05fadceff10e929c0
Kuudev <kuuhakuDeveloper@outlook.com>
2021-01-05 22:22:44 -0600

    Modify-Index.jsx-AddGaleryComponent

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,7 @@ import Header from "../util/componets/header"
 import Equipo from "../util/componets/equipos"
 import Rutinas from "../util/componets/rutinas"
 import Coach from "../util/componets/coach"
+import Galery from "../util/componets/galery"
 //import "../util/bootstrap/scss/bootstrap.scss"
 
 // markup
@@ -15,6 +16,7 @@ const IndexPage = () => {
       <Equipo/>
       <Rutinas/>
       <Coach/>
+      <Galery/>
     </main>
   )
 }
